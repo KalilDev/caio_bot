b9b69ca337f0c2b01da5eae65750600597e4fe86
KalilDev <kalilgamer0@gmail.com>
2019-12-09 01:05:47 -0300

    settings_view: Remove leaked data
    * I mean, i didn't actually removed it, but it is now invalidated and im taking it from an off-git file so that this doesn't happen again

--- lib/views/settings_view.dart
+++ lib/views/settings_view.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:caio_bot/bloc/twitter_bot_orchestrator/bloc.dart';
 import 'package:flutter_twitter_login/flutter_twitter_login.dart';
+import '../auth.dart';
 
 class SettingsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<BotOrchestratorBloc, BotOrchestratorState>(builder: (BuildContext context, BotOrchestratorState state) {
       return Center(child: RaisedButton(onPressed: () async {
-        final TwitterLoginResult result = await TwitterLogin(consumerKey: "yuido7C5UTc7eKbwx9NmaS02L", consumerSecret: "Z43ykEay1VU3y7LjN7CuqcAy6aaAn1qE28hpXiXXSYEHP8eSt7").authorize();
+        final TwitterLoginResult result = await TwitterLogin(consumerKey: credentials['consumerKey'], consumerSecret: credentials['consumerSecret']).authorize();
         print(result.session.toMap());
       },),);
     });
